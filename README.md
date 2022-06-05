67d5650d7026f7cad219d613b34eda0e12ee52c2
akki4410 <akhileswari712@gmail.com>
2022-06-05 16:48:01 +0530

    updated readmefile

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Graduate-Admission-Prediction
+
+Predict the chances of admission of a student .The dataset contaions parameters
+like GRE Score,TOEFL Score,University Rating, SOP,LOR ,CGPA,Research.
+
+I have predicted the chances of admission using Logistic Regression
+
+Implemented Steps:
+1. Import the Required Libraries
+2. Read the CSV file
+3. Preprocess the data
+4. Split the data (train & test)
+5. Build the Logistic Regression model
+6. Predict the test data
+7. Calculate the score of the model
+
--- main.py
+++ main.py
@@ -1,43 +1,44 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from matplotlib.colors import ListedColormap
 
+#read the csv file
 data = pd.read_csv("Admission_Predict.csv")
+
+#drop the serial column
 data.drop(["Serial No."], axis=1, inplace=True)
-x = data.iloc[:, [0, 1]].values
+x = data.iloc[:, 1].values
 y = data.iloc[:, 7].values
 
-from sklearn.cross_validation import train_test_split
-
+#split into train and test data
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=1 / 4, random_state=0)
-
 y_train1 = [1 if each > 0.8 else 0 for each in y_train]
 y_test1 = [1 if each > 0.8 else 0 for each in y_test]
 
+#convert ytrain1 and ytest1 into mnumpy arrays
 y_train1 = np.array(y_train1)
 y_test1 = np.array(y_test1)
 
-from sklearn.preprocessing import StandardScaler
-
+#Scale the data
 sc_x = StandardScaler()
 x_train = sc_x.fit_transform(x_train)
 x_test = sc_x.fit_transform(x_test)
 
-from sklearn.linear_model import LogisticRegression
-
+#Logistic regression
 log_reg = LogisticRegression(random_state=0)
 log_reg = log_reg.fit(x_train, y_train1)
 ypred = log_reg.predict(x_test)
 
-from sklearn.metrics import confusion_matrix
 
 cm = confusion_matrix(ypred, y_test1)
-
-from sklearn.metrics import accuracy_score
-
 acc = accuracy_score(ypred, y_test1)
 
-from matplotlib.colors import ListedColormap
 
 x_set, y_set = x_test, y_test1
 x1, x2 = np.meshgrid(np.arange(start=x_set[:, 0].min() - 1, stop=x_set[:, 0].max() + 1, step=0.01),
